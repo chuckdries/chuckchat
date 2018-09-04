f38dfd2144089f8269106324c9090f2c62f99fdc
Chuck Dries <chuck@chuckdries.com>
2018-09-04 00:48:05 -0700

    airbnb style modifications for client

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,33 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'Chuck',
+    };
+  }
+
+  changeName(name) {
+    this.setState({
+      name,
+    });
+  }
+
   render() {
+    const {
+      name,
+    } = this.state;
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React, {name}</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit <code>src/App.js </code> and save to reload.
+          <input type="test" onChange={e => this.changeName(e.target.value)} value={name} />
         </p>
       </div>
     );
